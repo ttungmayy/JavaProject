985923bc6464c58932c8117e85106d979289f926
namedojimo <32924409+namedojimo@users.noreply.github.com>
2017-11-14 00:04:28 +0700

    TM

--- Block.java
+++ Block.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import javafx.animation.AnimationTimer;
 import javafx.animation.PathTransition;
 import javafx.animation.TranslateTransition;
 import javafx.scene.image.Image;
@@ -15,29 +17,37 @@ import javafx.scene.shape.Path;
 import javafx.util.Duration;
 
 public class Block extends Pane{
-    int blockX = 100;
-    int blockY = 100;
+    int blockX = 50;
+    int blockY = 50;
     int blockWidth = 100;
     int blockHeight = 100;
+    int blockSpeed = 5;
     int totalBlock = 0;
     int score = 0;
-    int swingCount = 5;
-    int detachPosY = 650;
-    boolean gameOver = false;
-    TranslateTransition translateTransition = new TranslateTransition();
-    PathTransition pathTransition = new PathTransition();
+    boolean isDetach = false;
+    boolean isOver = false;
+    int count = 0;
+    int angle = 0;
+    int rotation = 0;
+    int radius = 1;
+    Path path;
+    PathTransition pathTransition;
+    AnimationTimer blockMovement;
+    InputStream block;
     ImageView imgBlock;
+    Block newBlock;
     
     public Block()
     {
         try {
-            InputStream block = Files.newInputStream(Paths.get("C:/Users/PM/Documents/NetBeansProjects/TowerBloxx/src/towerbloxx/box.png"));
-            imgBlock = new ImageView(new Image(block));
-            imgBlock.setFitWidth(blockWidth);
-            imgBlock.setFitHeight(blockHeight);
-            imgBlock.setTranslateX(blockX);
-            imgBlock.setTranslateY(blockY);
-            getChildren().add(imgBlock);
+        block = Files.newInputStream(Paths.get("/Users/namedojimo/NetBeansProjects/Project/src/towerbloxx/box.png"));
+        imgBlock = new ImageView(new Image(block));
+        imgBlock.setFitWidth(blockWidth);
+        imgBlock.setFitHeight(blockWidth);
+        imgBlock.setTranslateX(blockX);
+        imgBlock.setTranslateY(blockY);
+        
+        getChildren().add(imgBlock);
         } catch (IOException e)
         {
             System.out.println("Cannot load box.png");
@@ -45,35 +55,61 @@ public class Block extends Pane{
     }
     
     public void swing()
-    {
-        Path path = new Path();
-        MoveTo moveTo = new MoveTo(100, 0);
-        CubicCurveTo cubicCurveTo = new CubicCurveTo(100, 0, 200, 250, 700, 0);
-        
+    { 
+        path = new Path();
+        MoveTo moveTo = new MoveTo(0, 0);
+        CubicCurveTo cubicCurveTo = new CubicCurveTo(0, 0, 250, 250, 500, 0);
+
         path.getElements().add(moveTo);
         path.getElements().add(cubicCurveTo);
 
-        pathTransition.setDuration(Duration.millis(2000)); 
-        pathTransition.setNode(this);
+        pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(1000));
+         if (count == 0)
+        {
+            pathTransition.setNode(imgBlock);
+            count++;
+        }
+        else
+        {      
+            newBlock = new Block();
+            getChildren().add(newBlock); 
+            pathTransition.setNode(newBlock);
+        }
+        //pathTransition.setNode(imgBlock);
         pathTransition.setPath(path);
-        pathTransition.setCycleCount(swingCount);
+        pathTransition.setCycleCount(100);
+     
         pathTransition.setAutoReverse(true);
         pathTransition.play();
+        
+        
     }
-    
-    public void detach()
+// 
+    public void detach() 
     {
+        //isDetach = true;
         pathTransition.stop();
         totalBlock++;
         score += 10;
-        
+        blockY = (int) getTranslateY();
+        blockX = (int) getTranslateX();
+        TranslateTransition translateTransition = new TranslateTransition();
         translateTransition.setDuration(Duration.millis(1000));
-        translateTransition.setNode(imgBlock);
-        translateTransition.setToY(detachPosY);
+        if (count == 1)
+        {
+            translateTransition.setNode(imgBlock);
+            count++;
+        }
+        else
+        {      
+            translateTransition.setNode(newBlock);
+        }
+        
+        translateTransition.setToY(500);
         translateTransition.setCycleCount(1);
         translateTransition.setAutoReverse(true);
         translateTransition.play();
-<<<<<<< HEAD
 
         swing();
         
@@ -87,24 +123,5 @@ public class Block extends Pane{
         getChildren().add(newBlock); 
       
         return newBlock;
-=======
-        
-        detachPosY -= blockHeight;
-        makeNew();
-    }
-    
-    public void makeNew()
-    {
-        blockX = 100;
-        blockY = 100;
-        Block newBlock = new Block();
-        getChildren().add(newBlock);
-        newBlock.swing();
-    }
-    
-    public boolean isGameOver()
-    {
-        return gameOver;
->>>>>>> d0d6590050240bd56e129b27432e2b4dfecdf66f
     }
 }
