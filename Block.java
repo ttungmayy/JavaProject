ebf54a8e043cc52f06b754214186d1015ba1e7e5
ttungmayy <33589970+ttungmayy@users.noreply.github.com>
2017-11-13 17:17:10 +0700

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,106 @@
+package towerbloxx;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javafx.animation.AnimationTimer;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class Block extends Pane{
+    int blockX = 100;
+    int blockY = 100;
+    int blockWidth = 100;
+    int blockHeight = 100;
+    int blockSpeed = 5;
+    int totalBlock = 0;
+    int score = 0;
+    boolean isDetach = false;
+    
+    int angle = 0;
+    int rotation = 0;
+    int radius = 1;
+    AnimationTimer blockMovement;
+    
+    public Block()
+    {
+        try {
+        InputStream block = Files.newInputStream(Paths.get("C:/Users/PM/Documents/NetBeansProjects/TowerBloxx/src/towerbloxx/box.png"));
+        ImageView imgBlock = new ImageView(new Image(block));
+        imgBlock.setFitWidth(blockWidth);
+        imgBlock.setFitHeight(blockWidth);
+        imgBlock.setTranslateX(blockX);
+        imgBlock.setTranslateY(blockY);
+        getChildren().add(imgBlock);
+        } catch (IOException e)
+        {
+            System.out.println("Cannot load box.png");
+        }
+    }
+    
+    public void move()
+    {
+//        AnimationTimer blockMovement = new AnimationTimer()
+//        {
+//            public void handle(long arg0)
+//            {
+//                blockX += blockSpeed;
+//                if (blockX + blockWidth >= Main.width)
+//                {
+//                    blockX = Main.width - blockWidth;
+//                    blockSpeed *= -1;
+//                }
+//                else if (blockX + blockWidth < 0)
+//                {
+//                    blockX += blockWidth;
+//                    blockSpeed *= -1;
+//                }
+//                setTranslateX(blockX);
+//                setTranslateY(blockY);
+//            }
+//        };
+//        blockMovement.start();
+        
+        blockMovement = new AnimationTimer()
+        { 
+            public void handle(long arg0)
+            {
+                if (angle < 50)
+                {
+                    angle++;
+                    blockX += blockSpeed + Math.sin(Math.toRadians(angle)) * radius;
+                    blockY += blockSpeed + Math.cos(Math.toRadians(angle)) * radius;
+                }
+                
+                else
+                {
+                    angle++;
+                    blockX += blockSpeed + Math.cos(Math.toRadians(angle)) * radius;
+                    blockY -= blockSpeed + Math.sin(Math.toRadians(angle)) * radius;
+                }
+                
+                setTranslateX(blockX);
+                setTranslateY(blockY);
+            }
+        };
+        blockMovement.start();
+    }
+    
+    public void detach()
+    {
+        isDetach = true;
+        totalBlock++;
+        score += 10;
+        blockY += 50;
+    }
+    
+    public Block makeNew()
+    {
+        Block newBlock = new Block();
+        newBlock.blockX = 0;
+        isDetach = false;
+        return newBlock;
+    }
+}
