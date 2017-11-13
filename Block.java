b984e27ad46380cf92650aa2101c75a75b9a75c1
namedojimo <32924409+namedojimo@users.noreply.github.com>
2017-11-13 21:28:50 +0700

    drop dai leaw 2

--- Block.java
+++ Block.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 import javafx.animation.AnimationTimer;
 import javafx.animation.PathTransition;
+import javafx.animation.TranslateTransition;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -15,24 +17,27 @@ import javafx.scene.shape.Path;
 import javafx.util.Duration;
 
 public class Block extends Pane{
-    int blockX = 100;
-    int blockY = 100;
+    int blockX = 50;
+    int blockY = 50;
     int blockWidth = 100;
     int blockHeight = 100;
     int blockSpeed = 5;
     int totalBlock = 0;
     int score = 0;
     boolean isDetach = false;
+    boolean isOver = false;
     
     int angle = 0;
     int rotation = 0;
     int radius = 1;
+    Path path;
+    PathTransition pathTransition;
     AnimationTimer blockMovement;
     
     public Block()
     {
         try {
-        InputStream block = Files.newInputStream(Paths.get("C:/Users/PM/Documents/NetBeansProjects/TowerBloxx/src/towerbloxx/box.png"));
+        InputStream block = Files.newInputStream(Paths.get("/Users/namedojimo/NetBeansProjects/Project/src/towerbloxx/box.png"));
         ImageView imgBlock = new ImageView(new Image(block));
         imgBlock.setFitWidth(blockWidth);
         imgBlock.setFitHeight(blockWidth);
@@ -47,22 +52,22 @@ public class Block extends Pane{
     
     public void swing()
     {
-        Path path = new Path();
-        MoveTo moveTo = new MoveTo(100, 0);
-        CubicCurveTo cubicCurveTo = new CubicCurveTo(100, 0, 200, 250, 500, 0);
+        path = new Path();
+        MoveTo moveTo = new MoveTo(0, 0);
+        CubicCurveTo cubicCurveTo = new CubicCurveTo(0, 0, 250, 250, 500, 0);
 
         path.getElements().add(moveTo);
         path.getElements().add(cubicCurveTo);
 
-        PathTransition pathTransition = new PathTransition();
+        pathTransition = new PathTransition();
         pathTransition.setDuration(Duration.millis(2000)); 
         pathTransition.setNode(this);
         pathTransition.setPath(path);
-        pathTransition.setCycleCount(50);
+        pathTransition.setCycleCount(100);
+     
         pathTransition.setAutoReverse(true);
         pathTransition.play();
     }
-    
 //    public void move()
 //    {      
 //        blockMovement = new AnimationTimer()
@@ -88,14 +93,25 @@ public class Block extends Pane{
 //            }
 //        };
 //        blockMovement.start();
-//    }
-    
-    public void detach()
+//    }  
+    public void detach() throws InterruptedException
     {
         isDetach = true;
+        pathTransition.pause();
         totalBlock++;
         score += 10;
-        blockY += 50;
+        blockY = (int) getTranslateY();
+        blockX = (int) getTranslateX();
+        TranslateTransition translateTransition = new TranslateTransition();
+        translateTransition.setDuration(Duration.millis(2000));
+        translateTransition.setNode(this);
+        translateTransition.setToY(500);
+        translateTransition.setCycleCount(1);
+        translateTransition.setAutoReverse(true);
+        translateTransition.play();
+        //pathTransition.setDelay(Duration.millis(3000));
+        
+        swing();
     }
     
     public Block makeNew()
