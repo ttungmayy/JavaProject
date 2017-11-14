01ddf6cc6cf03009b203d72981ba5439a35922ab
namedojimo <32924409+namedojimo@users.noreply.github.com>
2017-11-14 11:28:42 +0700

    ต่อกันสิจ้ะ

--- Block.java
+++ Block.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
 import javafx.animation.AnimationTimer;
 import javafx.animation.PathTransition;
 import javafx.animation.TranslateTransition;
@@ -17,12 +16,13 @@ import javafx.scene.shape.Path;
 import javafx.util.Duration;
 
 public class Block extends Pane{
-    int blockX = 50;
-    int blockY = 50;
+    int blockX = 0;
+    int blockY = 0;
     int blockWidth = 100;
     int blockHeight = 100;
     int blockSpeed = 5;
     int totalBlock = 0;
+    int detachPosY = 600;
     int score = 0;
     boolean isDetach = false;
     boolean isOver = false;
@@ -36,6 +36,9 @@ public class Block extends Pane{
     InputStream block;
     ImageView imgBlock;
     Block newBlock;
+    
+    int posXOne = 0;
+    int posYOne = 0;
     
     public Block()
     {
@@ -57,71 +60,57 @@ public class Block extends Pane{
     public void swing()
     { 
         path = new Path();
-        MoveTo moveTo = new MoveTo(0, 0);
-        CubicCurveTo cubicCurveTo = new CubicCurveTo(0, 0, 250, 250, 500, 0);
+        MoveTo moveTo = new MoveTo(200, 50);
+        CubicCurveTo cubicCurveTo = new CubicCurveTo(200, 50, 250, 250, 700, 50);
 
         path.getElements().add(moveTo);
         path.getElements().add(cubicCurveTo);
 
         pathTransition = new PathTransition();
         pathTransition.setDuration(Duration.millis(1000));
-         if (count == 0)
-        {
+         if (count == 0){
             pathTransition.setNode(imgBlock);
             count++;
         }
-        else
-        {      
+        else{      
+             
             newBlock = new Block();
             getChildren().add(newBlock); 
             pathTransition.setNode(newBlock);
         }
-        //pathTransition.setNode(imgBlock);
         pathTransition.setPath(path);
         pathTransition.setCycleCount(100);
      
         pathTransition.setAutoReverse(true);
         pathTransition.play();
-        
-        
     }
 // 
     public void detach() 
     {
-        //isDetach = true;
+
         pathTransition.stop();
         totalBlock++;
         score += 10;
-        blockY = (int) getTranslateY();
-        blockX = (int) getTranslateX();
         TranslateTransition translateTransition = new TranslateTransition();
         translateTransition.setDuration(Duration.millis(1000));
-        if (count == 1)
-        {
+        if (count == 1){
             translateTransition.setNode(imgBlock);
             count++;
         }
-        else
-        {      
+        else{      
             translateTransition.setNode(newBlock);
         }
         
-        translateTransition.setToY(500);
+       translateTransition.setToY(detachPosY);
         translateTransition.setCycleCount(1);
         translateTransition.setAutoReverse(true);
         translateTransition.play();
+        detachPosY -= 83;
+        //CheckCollision();
 
         swing();
         
     }
       
-    public Block makeNew()
-    {    
-        newBlock = new Block();
-        newBlock.blockX = 100;
-        newBlock.blockY = 100;
-        getChildren().add(newBlock); 
-      
-        return newBlock;
-    }
+
 }
