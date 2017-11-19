e02c563a3804cb696cd03e6d47dfdbe67b2a659d
ttungmayy <33589970+ttungmayy@users.noreply.github.com>
2017-11-19 17:24:22 +0700

    happy song with how to play music

--- GameApp.java
+++ GameApp.java
@@ -1,43 +1,42 @@
 package towerbloxx;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import javafx.scene.image.Image;
+import java.io.File;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 public class GameApp extends Pane{
     Block block;
+    ImageView bg;
     
     public Pane runGameApp()
     {
         Pane game = new Pane();
         game.setPrefSize(Main.width , Main.height);
+
+        Media sound = new Media(new File("C:/Users/PM/Documents/NetBeansProjects/TowerBloxx/src/towerbloxx/happy.wav").toURI().toString());
+        MediaPlayer soundplay = new MediaPlayer(sound);
+        soundplay.setAutoPlay(true);
+        MediaView mediaview = new MediaView(soundplay);
+        game.getChildren().add(mediaview);
         
+       
         // background
-        try{
-        InputStream bg = Files.newInputStream(Paths.get("C:/Users/PM/Documents/NetBeansProjects/TowerBloxx/src/towerbloxx/town.jpg"));
-        ImageView imgBG = new ImageView(new Image(bg));
-        imgBG.setFitWidth(Main.width);
-        imgBG.setFitHeight(Main.height);
-        game.getChildren().add(imgBG);
-        } catch (IOException e)
-        {
-            System.out.println("Cannot load town.jpg");
-        }
+        bg = new Background().getBackground();
+        game.getChildren().add(bg);
         
         // block to play
         block = new Block();
         game.getChildren().add(block);
         
         // block swings back & forth
-        block.move();
+        block.swing();
         
         // detach block by clicking 
         game.setOnMouseClicked(event -> block.detach());
-        
+
         Main.root.getChildren().add(this);
         return game;
     }
